d5d494a961eaa51f3f5d87edc2edecfaa7ac9e40
SankalpMathur05 <sankalpmathur4@gmail.com>
2026-02-20 16:29:43 +0530

    Made changes to footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,15 +6,11 @@ const Footer = () => {
         <footer className="bg-background border-t border-border/40">
             <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
                 <div className="flex justify-center space-x-6">
-                    <Link href="#" className="text-gray-400 hover:text-gray-500">
-                        <span className="sr-only">Twitter</span>
-                        <Twitter className="h-6 w-6" />
-                    </Link>
-                    <Link href="#" className="text-gray-400 hover:text-gray-500">
+                    <Link href="https://github.com/SankalpMathur05/Smarrtif-Assignment-SkillNest" className="text-gray-400 hover:text-gray-500">
                         <span className="sr-only">GitHub</span>
                         <Github className="h-6 w-6" />
                     </Link>
-                    <Link href="#" className="text-gray-400 hover:text-gray-500">
+                    <Link href="https://www.linkedin.com/in/sankalp-mathur-985a171b0/" className="text-gray-400 hover:text-gray-500">
                         <span className="sr-only">LinkedIn</span>
                         <Linkedin className="h-6 w-6" />
                     </Link>
